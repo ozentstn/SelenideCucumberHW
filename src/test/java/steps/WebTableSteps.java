aa187abbff5a017807efad79c11ebaa34a72570d
ozentstn <ozentastan1@gmail.com>
2024-08-18 19:52:34 +0300

    HW17- test with cucumber - commit v3

--- src/test/java/steps/WebTableSteps.java
+++ src/test/java/steps/WebTableSteps.java
@@ -18,17 +18,10 @@ public class WebTableSteps extends BaseTest{
         assertThat(pageManager.webTablePage.webtables.isDisplayed()).isTrue();
         pageManager.webTablePage.webtables.click();
     }
-    @When("Click on the button marked +")
-    public void clickAddRecord(){
-        assertThat(pageManager.webTablePage.addRecord.isDisplayed()).isTrue();
-        pageManager.webTablePage.addRecord.click();
-    }
+
     @When("Enter fName, lName, eMail, age, salary, department and click on the submit")
-    public void detailData(io.cucumber.datatable.DataTable dT){
-        java.util.List<java.util.Map<String,String>> x=dT.asMaps(String.class, String.class);
-        for (java.util.Map<String,String> y:x){
+    public void detailData(){
             pageManager.webTablePage.InputData();
-        }
     }
     @When("Edit last record with inputData")
     public void editRecord(){
--- src/test/resources/features/WebTable.feature
+++ src/test/resources/features/WebTable.feature
@@ -3,6 +3,5 @@ Feature: Create and edit new records from webtables page
   Scenario: Click on the Web Tables button and add a record by clicking on the "+" icon.
     Given Webtables page opened
     When Click on the button named webtables
-    When Click on the button marked +
     When Enter fName, lName, eMail, age, salary, department and click on the submit
     When Edit last record with inputData
